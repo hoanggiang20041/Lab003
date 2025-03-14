83b1876d6f4c3f982771c638a31e88f50c6cd643
hoanggiang20041 <giang10012004@gmail.com>
2025-03-14 21:13:10 +0700

    Add files via upload

---
+++ csdl.sql
@@ -0,0 +1,27 @@
+﻿CREATE DATABASE Sanpham1;
+GO
+USE Sanpham1;
+GO
+
+CREATE TABLE Categories (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Products (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(255) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    Description NVARCHAR(MAX),
+    ImageUrl NVARCHAR(255),
+    CategoryId INT,
+    FOREIGN KEY (CategoryId) REFERENCES Categories(Id) ON DELETE CASCADE
+);
+
+CREATE TABLE ProductImages (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Url NVARCHAR(255) NOT NULL,
+    ProductId INT,
+    FOREIGN KEY (ProductId) REFERENCES Products(Id) ON DELETE CASCADE
+);
+
